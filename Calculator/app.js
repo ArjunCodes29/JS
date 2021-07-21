4291d2b509d5d80cd6769d4579647939f7f01341
ArjunCodes29 <lichessreck123@gmail.com>
2021-07-21 14:22:23 +0530

    finished html css started js

---
+++ Calculator/app.js
@@ -0,0 +1,26 @@
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const  add= document.getElementById('add');
+const substract = document.getElementById('substract');
+
+const one = document.getElementById('one');
+const  two = document.getElementById('two');
+const  three = document.getElementById('three');
+const  four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const  seven = document.getElementById('seven');
+const  eight = document.getElementById('eight');
+const  nine = document.getElementById('nine');
+const   zero= document.getElementById('zero');
+
+const  enter = document.getElementById('enter');
+
+const display = document.getElementById('display');
+
+
+one.addEventListener('click',function(){
+display.textContent = 1    
+
+    
+});
